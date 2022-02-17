2e6cc9f6cdf7e51247dee51fe852dd097288a858
arafin0 <sultanularafin000@gmail.com>
2022-02-17 17:28:07 +0600

    calculate button click event and calcucate reserve balance

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// html elements id capter 
 const income = document.getElementById('icome');
 const food = document.getElementById('food');
 const rent = document.getElementById('rent');
@@ -9,3 +10,31 @@ const persentes = document.getElementById('persentes');
 const saveBtn = document.getElementById('save');
 const savingAmount = document.getElementById('saving-amount');
 const remainingBalance = document.getElementById('remaining-balance');
+
+// calcutate button click and expanses amount calculate 
+calcutateBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    let incomeAmount =  parseFloat(income.value);
+    if(isNaN(incomeAmount) || incomeAmount<0 ){
+        return alert('Please input valid amount of money in number format');
+    }
+    let foodAmount = parseFloat(food.value);
+    let rentAmount = parseFloat(rent.value);
+    let colotheAmount = parseFloat(clothe.value);
+
+    if((isNaN(foodAmount) || foodAmount<0 ) || (isNaN(rentAmount) || rentAmount<0 )|| (isNaN(colotheAmount) || colotheAmount<0 )){
+        return alert('Please number format');
+    }
+    
+    let expansesAmount = foodAmount+rentAmount+colotheAmount;
+    if(expansesAmount>incomeAmount){
+        return alert('expenses amount gater then income');
+    }
+    totalExpanses.innerText=expansesAmount;
+
+    let resurvAmount = (incomeAmount- expansesAmount);
+    balance.innerText=resurvAmount;
+    food.value="";
+    rent.value="";
+    clothe.value="";
+})
